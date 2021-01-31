ebc58311941dc258738a4d13c8cff070eecc02d5
yijie4188 <yijie4188@gmail.com>
2021-02-01 00:01:01 +0800

    [fix] (static) lib

--- static/js/lib/tetra-script/index.js
+++ static/js/lib/tetra-script/index.js
@@ -1,7 +1,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
 define(["require", "exports", "./core/parser"], function (require, exports, parser_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
